0b177b699671acb3e2fa832b7cb39d902fee6649
trangwilliam <william.trangg@gmail.com>
2025-12-03 09:44:15 -0800

    day 3

---
+++ Day03/solution.py
@@ -0,0 +1,52 @@
+def part_1(fp: str) -> int:
+    with open(fp) as f:
+        total = 0
+        for line in f:
+            max_1 = -1
+            max_2 = -1
+
+            for i, digit in enumerate(line.strip()):
+                if (int(digit) > max_1) and (i != len(line.strip()) - 1):
+                    max_1 = int(digit)
+                    max_2 = -1
+                elif (int(digit) > max_2):
+                    max_2 = int(digit)
+            
+            total += max_1 * 10 + max_2
+    return total
+
+def part_2(fp:str, num_batteries:int=12) -> int:
+    with open(fp) as f:
+        total = 0
+        for line in f:
+            maxes = {i:-1 for i in range(num_batteries)}
+
+            for i, digit in enumerate(line.strip()):
+                check_index = len(line.strip()) - i - num_batteries
+                if check_index > 0:
+                    check_index = 0
+                else:
+                    check_index = abs(check_index)
+
+                for k, v in maxes.items():
+                    if k < check_index:
+                        continue
+                    if int(digit) > v:
+                        maxes[k] = int(digit)
+                        for j in range(k + 1, num_batteries):
+                            maxes[j] = -1
+                        break
+            
+            total += int(''.join([str(maxes[i]) for i in range(num_batteries)]))
+            
+    return total
+
+if __name__ == '__main__':
+    test_fp = './tests/test_input.txt'
+    input_fp = './tests/input.txt'
+
+    assert part_2(test_fp, 2) == 357
+    assert part_2(test_fp) == 3121910778619
+
+    print(part_1(input_fp))
+    print(part_2(input_fp))
